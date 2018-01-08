6a0cf3f4735c6612a176a1b77f795f11b942f069
adhurock <35215040+adhurock@users.noreply.github.com>
2018-01-08 01:08:52 -0800

    Initial commit

---
+++ app/src/main/java/com/example/pc/vtu/Sactivity.java
@@ -0,0 +1,13 @@
+package com.example.pc.vtu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sactivity);
+    }
+}
